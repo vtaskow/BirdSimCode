87b4712b08056e341c23994507687cb8e4cf20d2
Viktor Taskov <taskov_v@yahoo.com>
2017-02-26 22:04:36 +0000

    Adding code

---
+++ src/gla/joose/birdsim/Play.java
@@ -0,0 +1,31 @@
+package gla.joose.birdsim;
+
+import javax.swing.JFrame;
+
+import gla.joose.birdsim.boards.Board;
+import gla.joose.birdsim.boards.FlockBoard;
+import gla.joose.birdsim.boards.MovingForageBoard;
+import gla.joose.birdsim.boards.StaticForageBoard;
+
+
+/**
+ *  @author inah
+ *  The main method for bootstrapping BirdSim.
+ */
+public class Play extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		/* create a window for the game */
+		Play play = new Play();
+		
+		Board forageBoard = new StaticForageBoard(50,50);
+		forageBoard.initBoard(play);
+//		Board forageBoard = new MovingForageBoard(50,50);
+//		forageBoard.initBoard(play);
+		//Board simpleBoard = new FlockBoard(10,20);
+		//simpleBoard.initBoard(play);
+
+	}
+
+}
---
+++ src/gla/joose/birdsim/boards/FlockBoard.java
@@ -0,0 +1,103 @@
+package gla.joose.birdsim.boards;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * A BirdSim board with generic bird flying behavior.
+ */
+public class FlockBoard extends Board{
+	/* container for buttons and labels */
+    JPanel buttonPanel;
+    
+    /* buttons for hatching and scaring birds */
+    JButton hatchEggButton;
+    JButton scareBirdsButton;
+    /* displays current number of birds */
+    JLabel noOfBirdsLabel;
+    
+    /* not a fucking clue why this is here */
+    Thread runningthread;
+    
+    /* set board's dimensions */
+	public FlockBoard(int rows, int columns) {
+		super(rows, columns);		
+	}
+
+	
+	@Override
+	public void initBoard(final JFrame frame) {
+		/* get the main panel and put in in the window that contains the game */
+        JPanel display = getJPanel();
+        frame.getContentPane().add(display, BorderLayout.CENTER);
+        
+        /* add a panel for the buttons and the label*/
+        buttonPanel = new JPanel();
+        frame.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+        
+        hatchEggButton = new JButton("hatch egg");
+        buttonPanel.add(hatchEggButton);
+        hatchEggButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	scareBirds = false;
+            	runningthread = new Thread(new Runnable(){
+					public void run() {
+						fly();
+					}            		
+            	});
+            	runningthread.start();
+        }}); 
+
+        scareBirdsButton = new JButton("scare birds");
+        buttonPanel.add(scareBirdsButton);
+        scareBirdsButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	scareBirds = true;
+        }}); 
+        
+        noOfBirdsLabel = new JLabel();
+        noOfBirdsLabel.setText("#birds: "+0);
+        buttonPanel.add(noOfBirdsLabel);
+
+        
+        // Implement window close box
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+            	//used to invoke birds removal from the board
+            	scareBirds = true;
+            	if(runningthread !=null){
+                    clear();
+                    try {
+						runningthread.join();
+					} catch (InterruptedException e1) {
+						e1.printStackTrace();
+					}
+            	}
+            	frame.dispose();
+                System.exit(0);
+        }});
+        
+        frame.pack();
+        frame.setSize(650, 650);
+        frame.setVisible(true);
+        		
+	}
+
+	@Override
+	public void updateStockDisplay(){
+		updateStock();
+		noOfBirdsLabel.setText("#birds: "+noofbirds);
+	}
+	
+	
+	
+
+}
