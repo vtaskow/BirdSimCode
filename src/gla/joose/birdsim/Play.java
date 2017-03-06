8ba432c47816f549ef545a8d75c15c77ab395fc1
Mindaugas <2210401v@student.gla.ac.uk>
2017-03-06 21:20:19 +0000

    commandline selection of boards and behaviours

--- src/gla/joose/birdsim/Play.java
+++ src/gla/joose/birdsim/Play.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 
 import javax.swing.JFrame;
 
+import flybehaviors.DyingMultiplyingMovingForage;
+import flybehaviors.DyingMultiplyingStaticForage;
+import flybehaviors.DyingNoForage;
+import flybehaviors.DyingStaticForage;
+import flybehaviors.MovingForage;
+import flybehaviors.MultiplyingMovingForage;
+import flybehaviors.NoForage;
+import flybehaviors.StaticForage;
 import gla.joose.birdsim.boards.Board;
 import gla.joose.birdsim.boards.FlockBoard;
 import gla.joose.birdsim.boards.MovingForageBoard;
@@ -23,9 +31,14 @@ public class Play extends JFrame {
 	
 	public static void printBehaviourChoices(){
 		System.out.println("Choose a bird behaviour:");
-		System.out.println("\t 1) Birds die off when there is no grain, otherwise they multiply; Forage is moving");
-		System.out.println("\t 1) Birds die off when there is no grain, otherwise they multiply; Forage is static");
-		System.out.println("\t 1) Birds die off when there is no grain, otherwise they multiply; Forage is static");
+		System.out.println("\t 1) Moving Forage, Birds Die and Multiply");
+		System.out.println("\t 2) Static Forage, Birds Die and Multiply");
+		System.out.println("\t 3) No Forage, Birds Die");
+		System.out.println("\t 4) Static Forage, Birds Die");
+		System.out.println("\t 5) Moving Forage");
+		System.out.println("\t 6) Moving Forage, Birds Multiply");
+		System.out.println("\t 7) No Forage");
+		System.out.println("\t 8) Static Forage");
 	}
 	
 	public static void validateChoice(int limit, int choice){
@@ -40,52 +53,61 @@ public class Play extends JFrame {
 		Scanner input = new Scanner(System.in);
 		printBoardChoices();
 		System.out.print("--->");
-		int boardChoice = input.nextInt();
-		validateChoice(3, boardChoice); // we have three kinds of boards that we choose from 
-
+		int Choice = input.nextInt();
+		validateChoice(3, Choice); // we have three kinds of boards that we choose from 
+		
+		
 		/* create a window for the game */
 		Play play = new Play();
 		
-		Board generalBoard;
+		Board generalBoard = new FlockBoard(20, 20); // need to do this 'dummy board' as otherwise java cries about this not being initialised 
 		// generalBoard may not have been initialized
-		switch (boardChoice) {
+		switch (Choice) {
 		case 1:
 			generalBoard = new FlockBoard(50, 50);
-			generalBoard.initBoard(play);
 			break;
 		case 2:
 			generalBoard = new StaticForageBoard(50, 50);
-			generalBoard.initBoard(play);
 			break;
 		case 3:
 			generalBoard = new MovingForageBoard(50, 50);
-			generalBoard.initBoard(play);
 			break;
 		}
+		//generalBoard.initBoard(play);
 		
-		/*Board forageBoard = new StaticForageBoard(50,50);
-		forageBoard.initBoard(play);
-		forageBoard.setFlyBehavior(new DyingNoForage(forageBoard));
-		forageBoard.initBoard(play);*/
+		printBehaviourChoices();
+		System.out.print("--->");
+		Choice = input.nextInt();
+		validateChoice(8, Choice); // we have eight kinds of behaviours that we choose from
+		input.close();
 		
-		/*Board forageBoard = new MovingForageBoard(50, 50);
-		forageBoard.setFlyBehavior(new DyingNoForage(forageBoard));
-		forageBoard.initBoard(play);
-		forageBoard.setFlyBehavior(new StaticForage(forageBoard));*/
-		
-		/*Board simpleBoard = new FlockBoard(10, 20);
-		simpleBoard.setFlyBehavior(new DyingMultiplyingMovingForage(simpleBoard));
-		simpleBoard.initBoard(play);*/
-		
-		/*Board forageBoard = new StaticForageBoard(50, 50);
-		forageBoard.setFlyBehavior(new DyingStaticForage(forageBoard));
-		forageBoard.setFlyBehavior(new DyingMultiplyingMovingForage(forageBoard));
-		forageBoard.initBoard(play);*/
-		
-		//Board forageBoard = new MovingForageBoard(50, 50);
-		//forageBoard.setFlyBehavior(new MultiplyingMovingForage(forageBoard));
-		//forageBoard.initBoard(play);
-
+		switch (Choice){
+		case 1:
+			generalBoard.setFlyBehavior(new DyingMultiplyingMovingForage(generalBoard));
+			break;
+		case 2:
+			generalBoard.setFlyBehavior(new DyingMultiplyingStaticForage(generalBoard));
+			break;
+		case 3:
+			generalBoard.setFlyBehavior(new DyingNoForage(generalBoard));
+			break;
+		case 4:
+			generalBoard.setFlyBehavior(new DyingStaticForage(generalBoard));
+			break;
+		case 5:
+			generalBoard.setFlyBehavior(new MovingForage(generalBoard));
+			break;
+		case 6:
+			generalBoard.setFlyBehavior(new MultiplyingMovingForage(generalBoard));
+			break;
+		case 7:
+			generalBoard.setFlyBehavior(new NoForage(generalBoard));
+			break;
+		case 8:
+			generalBoard.setFlyBehavior(new StaticForage(generalBoard));
+			break;
+		}
+		generalBoard.initBoard(play);
 	}
 
 }
